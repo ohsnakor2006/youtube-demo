0aa6f7a6b08bdc3ce95b14ee11fce19b00e5bc23
hanfei <hanfeichen123@gmail.com>
2020-06-25 14:12:06 +0800

    rename, modified

--- youTubeProject/ConstantsTemplate.swift
+++ youTubeProject/ConstantsTemplate.swift
@@ -1,18 +1,19 @@
 //
-//  Constants.swift
+//  ConstantsTemplate.swift
 //  youTubeProject
 //
-//  Created by yuguo on 23/6/20.
+//  Created by yuguo on 25/6/20.
 //  Copyright © 2020 RY. All rights reserved.
 //
 
 import Foundation
 
-struct Constants {
+/*struct Constants {
     
-     static var API_KEY = "AIzaSyDa79ycGKFY9OR2VWek07IY2vFxq2DLEJw"
-    static var PLAYLIST_ID = "PL4yIEp7Aojj7dppbuS1lILAVpPRifO9VN"
+     static var API_KEY = ""
+    static var PLAYLIST_ID = ""
     static var API_URL = "'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
     
 }
+ */
